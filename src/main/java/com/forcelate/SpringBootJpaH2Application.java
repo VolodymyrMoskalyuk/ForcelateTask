0f3406f09aaa257602dc4ec015c38b49d9150871
Volodymyr <pitbullboks@gmail.com>
2018-06-27 22:23:09 +0300

    Update SpringBootJpaH2Application.java

--- src/main/java/com/forcelate/SpringBootJpaH2Application.java
+++ src/main/java/com/forcelate/SpringBootJpaH2Application.java
@@ -18,29 +18,29 @@ public class SpringBootJpaH2Application {
         SpringApplication.run(SpringBootJpaH2Application.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner runner(UserRepository userRepository, ArticleRepository articleRepository) {
-//        return args -> {
-//            userRepository.save(new User("Pavlo", 22));
-//            userRepository.save(new User("Petro", 20));
-//            userRepository.save(new User("Ivan", 30));
-//            userRepository.save(new User("Oleh", 35));
-//            userRepository.save(new User("Adam", 24));
-//            userRepository.save(new User("Roman", 28));
-//            userRepository.save(new User("Igor", 19));
-//
-//            articleRepository.save(new Article("6 Instant Confidence Boosters", GREEN, userRepository.getOne(Long.valueOf(1))));
-//            articleRepository.save(new Article("Parenting Guru: From Chaos to Access", RED, userRepository.getOne(Long.valueOf(2))));
-//            articleRepository.save(new Article("Rob Banks Legally", BLUE, userRepository.getOne(Long.valueOf(1))));
-//            articleRepository.save(new Article("Are You Too Clever For Success?", GREEN, userRepository.getOne(Long.valueOf(3))));
-//            articleRepository.save(new Article("Six Types Of Investors – Which Group Are You In?", RED, userRepository.getOne(Long.valueOf(1))));
-//            articleRepository.save(new Article("How I Doubled My Money With Facebook Ads", BLUE, userRepository.getOne(Long.valueOf(4))));
-//            articleRepository.save(new Article("For At Risk Youths, Learning Digital Media Is A Luxury", YELLOW, userRepository.getOne(Long.valueOf(5))));
-//            articleRepository.save(new Article("The Ultimate Guide to Making Money Online", PINK, userRepository.getOne(Long.valueOf(1))));
-//            articleRepository.save(new Article("21 Ways to Dominate Youtube: The Ultimate Guide", GREY, userRepository.getOne(Long.valueOf(6))));
-//            articleRepository.save(new Article("Thousands Already Make Millions Online And So Can You", BROWN, userRepository.getOne(Long.valueOf(6))));
-//            articleRepository.save(new Article("No Cellphones – By Law", BLACK, userRepository.getOne(Long.valueOf(7))));
-//        };
-//    }
+   @Bean
+   CommandLineRunner runner(UserRepository userRepository, ArticleRepository articleRepository) {
+       return args -> {
+           userRepository.save(new User("Pavlo", 22));
+           userRepository.save(new User("Petro", 20));
+           userRepository.save(new User("Ivan", 30));
+           userRepository.save(new User("Oleh", 35));
+           userRepository.save(new User("Adam", 24));
+           userRepository.save(new User("Roman", 28));
+           userRepository.save(new User("Igor", 19));
+
+           articleRepository.save(new Article("6 Instant Confidence Boosters", GREEN, userRepository.getOne(Long.valueOf(1))));
+           articleRepository.save(new Article("Parenting Guru: From Chaos to Access", RED, userRepository.getOne(Long.valueOf(2))));
+           articleRepository.save(new Article("Rob Banks Legally", BLUE, userRepository.getOne(Long.valueOf(1))));
+           articleRepository.save(new Article("Are You Too Clever For Success?", GREEN, userRepository.getOne(Long.valueOf(3))));
+           articleRepository.save(new Article("Six Types Of Investors – Which Group Are You In?", RED, userRepository.getOne(Long.valueOf(1))));
+           articleRepository.save(new Article("How I Doubled My Money With Facebook Ads", BLUE, userRepository.getOne(Long.valueOf(4))));
+           articleRepository.save(new Article("For At Risk Youths, Learning Digital Media Is A Luxury", YELLOW, userRepository.getOne(Long.valueOf(5))));
+           articleRepository.save(new Article("The Ultimate Guide to Making Money Online", PINK, userRepository.getOne(Long.valueOf(1))));
+           articleRepository.save(new Article("21 Ways to Dominate Youtube: The Ultimate Guide", GREY, userRepository.getOne(Long.valueOf(6))));
+           articleRepository.save(new Article("Thousands Already Make Millions Online And So Can You", BROWN, userRepository.getOne(Long.valueOf(6))));
+           articleRepository.save(new Article("No Cellphones – By Law", BLACK, userRepository.getOne(Long.valueOf(7))));
+       };
+   }
 
 }
